e9ee70d3e1a885fb6a6d41f2694b3969f650e55b
Giorgi kirvalidze <giorgikirvalidzee@gmail.com>
2020-09-17 12:51:52 +0400

    add about text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,7 @@ const request = require('request')
 
 
 const forecast = (lat, long, callback) => {
-    const url = `http://api.weatherstack.com/current?access_key=299bf1ae18d0c8952381296534dccfd0&query=${lat},${long}&units=s`
+    const url = `http://api.weatherstack.com/current?access_key=299bf1ae18d0c8952381296534dccfd0&query=${lat},${long}&units=f`
     request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('unable to connect to weather service!', undefined)
